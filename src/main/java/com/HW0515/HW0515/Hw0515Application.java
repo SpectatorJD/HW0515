b94d15a827f42121d20d7727fa93e58ce758ea23
Tkakishak <tkakishakd@mail.ru>
2023-05-15 22:42:11 +0300

    Initial commit

---
+++ src/main/java/com/HW0515/HW0515/Hw0515Application.java
@@ -0,0 +1,13 @@
+package com.HW0515.HW0515;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw0515Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw0515Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HW0515/HW0515/Hw0515ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW0515.HW0515;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw0515ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
